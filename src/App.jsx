cc00e4b7c480b6a0087991322dbb7156ba190917
Czar Anthony <czaranthony51@gmail.com>
2026-07-22 20:33:50 +0800

    learning localStorage simple project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,72 @@
 import { useState } from "react";
-import Education from "./components.jsx/Education";
-import GeneralInfo from "./components.jsx/GeneralInfo";
-import Experience from "./components.jsx/Experience";
-import { GeneInfo, ExperienceInfo, EducationInfo } from "./data/InfoData";
 
-function App() {
-  const [genInfo, setGenInfo] = useState(GeneInfo);
+function ThemeColor() {
+  const [settings, setSettings] = useState(() => {
+    const savedUser = localStorage.getItem("settings");
 
-  const [ExpInfo, setExInfo] = useState(ExperienceInfo);
+    if (savedUser) {
+      return JSON.parse(savedUser);
+    }
+    return { username: "", theme: "Light", notifications: false };
+  });
 
-  const [EducInfo, setEducInfo] = useState(EducationInfo);
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    setSettings({
+      ...settings,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+  const SavedSetting = () => {
+    localStorage.setItem("settings", JSON.stringify(settings));
+  };
+
+  const Clear = () => {
+    localStorage.removeItem("settings");
+
+    setSettings({
+      username: "",
+      theme: "Light",
+      notifications: false,
+    });
+  };
 
   return (
     <div>
-      <h1>CV Application</h1>
-      <GeneralInfo genInfo={genInfo} setGenInfo={setEducInfo} />
-      <Education EducInfo={EducInfo} setEducInfo={setEducInfo} />
+      <input
+        type="text"
+        name="username"
+        placeholder="Enter name"
+        onChange={handleChange}
+        value={settings.username}
+      />
 
-      <Experience ExpInfo={ExpInfo} setExInfo={setExInfo} />
+      <select onChange={handleChange} value={settings.theme} name="theme">
+        <option>Light</option>
+        <option>Dark</option>
+      </select>
+
+      <input
+        type="checkbox"
+        name="notifications"
+        onChange={handleChange}
+        checked={settings.notifications}
+      />
+      <button onClick={SavedSetting}>Save</button>
+      <button onClick={Clear}> Delete</button>
+
+      <h2>Current Settings</h2>
+
+      <p>Username: {settings.username}</p>
+
+      <p>Theme: {settings.theme}</p>
+
+      <p>
+        Notifications:
+        {settings.notifications ? " Enabled" : " Disabled"}
+      </p>
     </div>
   );
 }
-export default App;
+export default ThemeColor;
